07dd22d8c10e0f76e8396f82b1e49e2f3e0ea390
KarinaSkirmante <karina.krinkele@gmail.com>
2026-03-06 17:37:02 +0200

    Add: added mainService with main function and testt for the MyArrayList

---
+++ src/service/MainService.java
@@ -0,0 +1,26 @@
+package service;
+
+import datastr.MyArrayList;
+
+public class MainService {
+
+	public static void main(String[] args) {
+		MyArrayList symbols = new MyArrayList(2);
+		symbols.add('a');//a
+		symbols.add('b');//a b
+		symbols.add('c');//a b c (automātiski būs resize izpildījies)
+		try
+		{
+			symbols.print();//a b c
+			symbols.add('Z', 1);//a Z b c
+			symbols.print();//a Z b c
+			symbols.remove(2);//a Z c (jeb b tiek izdzēsts)
+			symbols.print();//a Z c
+		}
+		catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+
+	}
+
+}
